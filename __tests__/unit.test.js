e17dce9d202b672bdaff850af19b6d01e2cdb1e4
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2024-03-22 12:23:17 -0500

    replicate tpen-services testing set up

---
+++ __tests__/unit.test.js
@@ -0,0 +1,5 @@
+describe('Manifest endpoint functionality unit test (just testing helper functions). #functions_unit', () => {
+  it('A stub for tests within the TPEN3 Users Workflow', () => {
+    expect("hello world").toBe("hello world")
+  })
+}) 
